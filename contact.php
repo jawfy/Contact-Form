9ae65aa196e24232dd191e0b39c3afb183f7f2f3
Jawfy <me@jackearsman.com>
2016-07-06 15:20:19 +0100

    test

--- contact.php
+++ contact.php
@@ -3,5 +3,9 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 
-echo $name . "" $email . "" $message . ""
+$to = 'me@jackearsman.com';
+$subject = 'New Message!';
+$msg = $name . '' . $email . '' . $message . '';
+
+$mail($to, $subject, $message);
 ?>
